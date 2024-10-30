fa26082ef66ab17af8497a095dc3c8c05430b9b7
ravindrareddy8 <ravindrareddysiemens@gmail.com>
2024-10-30 12:28:34 +0530

    python

---
+++ 234.py
@@ -0,0 +1,38 @@
+name = input("enter your name :")
+print("Welcome",name)
+# Write a Program to input 2 numbers & print their sum
+first = int(input("first number :"))
+second = int(input("second number :"))
+print("sume=", first + second)
+
+# Write a program to input side of square & print its area
+side = float(input("enter square side :"))
+print("sum =", side * side) #or
+print("sum =", side **2)
+
+first = int(input("enter first :"))
+second = int(input("enter second :"))
+print("sum =", first + second)
+
+side1 = float(input("enter square side1 :"))
+side2 = float(input("enter square side2 :"))
+print("Total area size :",side1 * side2)
+
+name = input("Enter your name:")
+print("Welcome", name)
+
+first = int(input("first value :"))
+second = int(input("second value :"))
+print("sum=:", first + second)
+
+# Write a program to input s2 floating point numbers & print their average
+a = float(input("first number :"))
+b = float(input("second number :"))
+print("Average :", (a+b)/2)
+
+# Write a program to input 2 int numbers, a and b
+#Print True if a is greater than or equal to b. If not print False
+    #(a >= b)  true
+a = int(input("first number :"))
+b = int(input("second number :"))
+print(a>=b)
